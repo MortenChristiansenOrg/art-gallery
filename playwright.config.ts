b45a8d13bca7b91a8676789859d8c8eda6a7831a
Morten Christiansen <withoutprecedence@gmail.com>
2026-01-24 12:32:31 +0100

    Use local convex in pipeline

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./e2e",
+  globalTimeout: process.env.CI ? 300000 : undefined,
   timeout: 10000,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
